6dffd1dd29e77feb40b3e1c2c7ddc5ecfdd66bdb
snakehead007 <snakehead007@protonmail.com>
2019-02-15 14:57:56 +0100

    modified:   install.sh 	modified:   ucll-vpn

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-echo "give your student username:" 
+echo "\nGive your student username:" 
 read user
 echo "setting up for ${user}..."
+#Potential improvments:
+#check if $user is a real student number 
 mkdir /usr/local/bin/ucll-vpn
 echo "installing in /usr/local/bin/ucll-vpn"
 echo ${user} > /usr/local/bin/ucll-vpn/user.txt 
 cp ./ucll-vpn /usr/bin
 chmod +x /usr/bin/ucll-vpn
-echo "DONE!"
-echo "type 'ucll-vpn' to run"
+echo "DONE: 'ucll-vpn' to run"
